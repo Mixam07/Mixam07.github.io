f548c6d1fa7c33ec247b6091b1e787cd05794755
Maksym Syvak <88047057+Mixam07@users.noreply.github.com>
2024-09-15 18:27:04 +0300

    Add files via upload

---
+++ web-1/index.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const body = document.querySelector("body");
+
+    body.innerHTML += `<div>Цей текст добавлений за допомогою JS</div>`
+});
